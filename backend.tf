b1eb4b7a8688f1c57ad6c18a7235ca078166c90c
Chucks Nwosu <chucksn611@gmail.com>
2024-02-21 10:58:15 -0600

    first commit

---
+++ backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  cloud {
+    organization = "chucksn611"
+
+    workspaces {
+      name = "dev"
+    }
+  }
+
+   required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.37.0"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region-1" {
+  type = string
+  description = "aws first region of choice"
+}
+
+variable "region-2" {
+  type = string
+  description = "aws second region of choice"
+}
